af79f7b2096cbb3f20a5d94a815f6ab6df7ce552
Emily Amorim <158009884+emiamorim@users.noreply.github.com>
2025-11-29 09:40:46 -0300

    Inserção Poligono

--- src/meupaint/gui/geom/Poligono.java
+++ src/meupaint/gui/geom/Poligono.java
@@ -27,14 +27,24 @@ public class Poligono extends Forma {
         double raio = Math.hypot( x2 - x1, y2 - y1 );
         double angulo = Math.atan2( y2 - y1, x2 - x1 );
         
-        // desenho de um círculo só para mostrar algo na tela
-        g2d.setColor(  corContorno );
-        g2d.drawOval( x1 - (int) raio, y1 - (int) raio, (int) raio * 2, (int) raio * 2 );
-        g2d.drawString( String.format( "polígono %d lados", quantidadeLados ), x1, y1 );
+        int centroX = x1;
+        int centroY= y1;
         
+        double incrementoAngulo = 2.0 * Math.PI / quantidadeLados;
         
-        // sua implementação aqui
+        for (int i = 0; i < quantidadeLados; i++) {
+            
+            double novoAngulo =  angulo + i * incrementoAngulo;
+       
+            x[i] = centroX + (int) ( raio * Math.cos( novoAngulo) );
+            y[i] = centroY + (int) ( raio * Math.sin( novoAngulo) );
+        }
         
+        g2d.setColor( corPreenchimento );
+        g2d.fillPolygon( x, y, quantidadeLados );
+        
+        g2d.setColor( corContorno );
+        g2d.drawPolygon( x, y, quantidadeLados );
         
         g2d.dispose();
         
